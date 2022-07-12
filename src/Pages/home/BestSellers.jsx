17f8dc0d19e3e407f83365981b40aac77bbbbfa8
Tushar <tusharkumar0807@gmail.com>
2022-07-12 22:49:55 +0530

    mobile view complete

--- src/Pages/home/BestSellers.jsx
+++ src/Pages/home/BestSellers.jsx
@@ -3,25 +3,27 @@ import React from "react";
 const BestSellers=() => {
   return <div className="flex flex-col">
     <div className="flex items-center justify-center text-xl font-bold my-3"><p>Shopping tips from our Editors</p></div>
-    <div className="flex flex-col">
-      <div className="flex w-full mx-3 my-5 justify-center ">
-        <div className="flex flex-col flex-1 justify-center">
-          <img src="https://i5.walmartimages.com/dfw/8822aef6-211b/k2-_12745a89-7dc0-4eb3-8eb1-47571774a9ce.v1.png" width="166" alt="" />
-          <p className="flex justify-center w-44"> Back-to-school ideas</p>
+    <div className="flex justify-center">
+      <div className="flex flex-col">
+        <div className="flex w-full mx-3 my-5 justify-center ">
+          <div className="flex flex-col flex-1 justify-center">
+            <img src="https://i5.walmartimages.com/dfw/8822aef6-211b/k2-_12745a89-7dc0-4eb3-8eb1-47571774a9ce.v1.png" width="166" alt="" />
+            <p className="flex justify-center w-44"> Back-to-school ideas</p>
+          </div>
+          <div className="flex flex-col flex-1 justify-center">
+            <img src="https://i5.walmartimages.com/dfw/8822aef6-cd88/k2-_ae400a6f-0684-4189-9285-f41380230132.v1.png" width="166" alt="" />
+            <p className="flex w-44 mr-auto justify-center">Summer camp checklist</p>
+          </div>
         </div>
-        <div className="flex flex-col flex-1 justify-center">
-          <img src="https://i5.walmartimages.com/dfw/8822aef6-cd88/k2-_ae400a6f-0684-4189-9285-f41380230132.v1.png" width="166" alt="" />
-          <p className="flex w-44 mr-auto justify-center">Summer camp checklist</p>
-        </div>
-      </div>
-      <div className="flex w-full mx-3 my-5 justify-center">
-        <div className="flex flex-col flex-1 justify-center">
-          <img src="https://i5.walmartimages.com/dfw/8822aef6-67b8/k2-_b0ad48df-2bfe-4887-baca-28a116458b68.v1.png" width="166" alt="" />
-          <p className="flex w-44 justify-center">  2022's best laptops</p>
-        </div>
-        <div className="flex flex-col flex-1 justify-center">
-          <img src="https://i5.walmartimages.com/dfw/8822aef6-c0ab/k2-_00b9ce34-df97-47b7-8c52-884da18b43c3.v1.png" width="166" alt="" />
-          <p className="flex w-44 mr-auto justify-center">  How to camp with dogs</p>
+        <div className="flex w-full mx-3 my-5 justify-center">
+          <div className="flex flex-col flex-1 justify-center">
+            <img src="https://i5.walmartimages.com/dfw/8822aef6-67b8/k2-_b0ad48df-2bfe-4887-baca-28a116458b68.v1.png" width="166" alt="" />
+            <p className="flex w-44 justify-center">  2022's best laptops</p>
+          </div>
+          <div className="flex flex-col flex-1 justify-center">
+            <img src="https://i5.walmartimages.com/dfw/8822aef6-c0ab/k2-_00b9ce34-df97-47b7-8c52-884da18b43c3.v1.png" width="166" alt="" />
+            <p className="flex w-44 mr-auto justify-center">  How to camp with dogs</p>
+          </div>
         </div>
       </div>
     </div>
--- src/Pages/home/RollbackItem.jsx
+++ src/Pages/home/RollbackItem.jsx
@@ -1,7 +1,46 @@
 import React from "react";
 
-const RollbackItem = () => {
-  return <div>RollbackItem</div>;
+const RollbackItem=() => {
+  return (
+    <div className="flex flex-col my-10">
+      <p className="font-medium text-xl my-4">Bestselling items on Rollback</p>
+      <div className="flex w-full my-5 overflow-scroll">
+        <div className="flex mr-3 mx-3 flex-grow-0 flex-shrink-0 flex-basis-auto w-44 flex-col overflow-x-auto">
+          <div className="flex h-32 justify-center items-center bg-red-50">Image</div>
+          <div className="flex justify-center">Name</div>
+          <div className="flex justify-center">Type</div>
+          <div className="flex justify-center">Price</div>
+          <div className="flex justify-center">Mode</div>
+          <button className="flex text-white bg-[#0096FF] justify-center items-center rounded-lg w-40">Add to cart</button>
+        </div>
+        <div className="flex mr-3 mx-3 flex-grow-0 flex-shrink-0 flex-basis-auto w-44 flex-col overflow-x-auto">
+          <div className="flex h-32 justify-center items-center bg-red-50">Image</div>
+          <div className="flex justify-center">Name</div>
+          <div className="flex justify-center">Type</div>
+          <div className="flex justify-center">Price</div>
+          <div className="flex justify-center">Mode</div>
+          <button className="flex text-white bg-[#0096FF] justify-center items-center rounded-lg w-40">Add to cart</button>
+        </div>
+        <div className="flex mr-3 mx-3 flex-grow-0 flex-shrink-0 flex-basis-auto w-44 flex-col overflow-x-auto">
+          <div className="flex h-32 justify-center items-center bg-red-50">Image</div>
+          <div className="flex justify-center">Name</div>
+          <div className="flex justify-center">Type</div>
+          <div className="flex justify-center">Price</div>
+          <div className="flex justify-center">Mode</div>
+          <button className="flex bg-[#0096FF] justify-center items-center rounded-lg w-40">Add to cart</button>
+        </div>
+        <div className="flex mr-3 mx-3 flex-grow-0 flex-shrink-0 flex-basis-auto w-44 flex-col overflow-x-auto">
+          <div className="flex h-32 justify-center items-center bg-red-50">Image</div>
+          <div className="flex justify-center">Name</div>
+          <div className="flex justify-center">Type</div>
+          <div className="flex justify-center">Price</div>
+          <div className="flex justify-center">Mode</div>
+          <button className="flex  bg-[#0096FF] justify-center items-center rounded-lg w-40">Add to cart</button>
+        </div>
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default RollbackItem;
